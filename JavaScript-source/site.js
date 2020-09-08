6ac0be0236a0433156142e75d14d5e94b934f737
ConnilQuentin <37124198+ConnilQuentin@users.noreply.github.com>
2020-09-08 14:07:31 +0200

    first commit

---
+++ JavaScript-source/site.js
@@ -0,0 +1,37 @@
+var a = document.getElementsByClassName("slideimg1")
+var b = document.getElementsByClassName("slideimg2")
+var c = document.getElementsByClassName("slideimg3")
+
+function changeDivs(n){
+
+}
+function goto(n){
+
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-red", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-red";
+}
